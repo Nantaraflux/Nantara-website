cd2ddc22f2e4a2f570ff1c4a7e1cb642820b94c7
Nantaraflux <matthewbryantan@gmail.com>
2026-09-15 23:14:11 +0700

    Route demo and consultation buttons to WhatsApp

--- site-config.js
+++ site-config.js
@@ -1,2 +1,2 @@
 // Set the real GA4 measurement ID before publishing. No analytics run without consent.
-window.NANTARA_CONFIG={ga4Id:'',bookingUrl:'https://calendly.com/nantara',whatsappUrl:'https://wa.me/message/7DRPFHKTWGPDE1'};
+window.NANTARA_CONFIG={ga4Id:'',bookingUrl:'https://wa.me/message/7DRPFHKTWGPDE1',whatsappUrl:'https://wa.me/message/7DRPFHKTWGPDE1'};
